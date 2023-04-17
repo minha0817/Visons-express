941ff941fd39169ddfaa05798b352a10555193c7
minha0817 <minhakk24@gmail.com>
2023-04-16 23:48:18 -0700

    Set up mysql

--- index.ts
+++ index.ts
@@ -1,39 +1,79 @@
 import express from "express";
 import cors from "cors";
 import fs from "fs";
+import mysql from "mysql";
+import { faker } from "@faker-js/faker";
+import dotenv from 'dotenv'
+dotenv.config()
 
-const app = express();
+// const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cors());
 
-app.get("/api", (req, res, next) => {
-    
-    res.send(fs.readFileSync("./data.json", "utf8"));
+// app.get("/api", (req, res, next) => {
 
+//     res.send(fs.readFileSync("./data.json", "utf8"));
+
+// });
+
+// app.post("/api", (req, res) => {
+//     const date = req.body.clickedDate;
+//     const habit = req.body.input;
+
+//     const data = fs.readFileSync("./data.json", "utf8");
+//     const dataObj = JSON.parse(data);
+
+//     if (dataObj[date]) {
+//     dataObj[date].push(habit);
+//     } else {
+//     dataObj[date] = [habit];
+//     }
+
+//     fs.writeFileSync("./data.json", JSON.stringify(dataObj, null, 2), 'utf8')
+
+//     console.log("dataObj:", dataObj);
+//     res.end();
+// });
+
+// const port = +(process.env.PORT || 8443);
+// app.listen(port, () => {
+//     console.log(`Express Server started on port ${port}`);
+// });
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PASSWORD,
+  database: "VisonZ_Practice",
 });
 
-app.post("/api", (req, res) => {
-    const date = req.body.clickedDate;
-    const habit = req.body.input;
+// const sql = mysql.format("SELECT * FROM user", []);
 
-    const data = fs.readFileSync("./data.json", "utf8");
-    const dataObj = JSON.parse(data);
+const values = [];
+for (let i = 0; i < 50; i++) {
+  values.push([
+    faker.address.streetAddress(),
+    faker.address.city(),
+    faker.address.country(),
+    i+1
+  ]);
+}
 
-    if (dataObj[date]) {
-    dataObj[date].push(habit);
-    } else {
-    dataObj[date] = [habit];
-    }
+const sql = mysql.format(
+  "INSERT INTO user_address (street_address, city, country, user_id) VALUES ?",
+  [values]
+);
 
-    fs.writeFileSync("./data.json", JSON.stringify(dataObj, null, 2), 'utf8')
+console.log('result of sql', sql);
 
-    console.log("dataObj:", dataObj);
-    res.end();
-});
+connection.query(sql, (err, results) => {
+  if (err) throw err;
+    console.log('results', results);
+//   results.forEach((result: any) => {
+//     console.log("result.username", result.username);
+//   });
 
-const port = +(process.env.PORT || 8443);
-app.listen(port, () => {
-    console.log(`Express Server started on port ${port}`);
+  connection.end();
 });
