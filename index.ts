29e410cdda881cb7639bff7d4d23d1ad92258925
minha0817 <minhakk24@gmail.com>
2023-04-23 18:52:03 -0700

    connect to db

--- index.ts
+++ index.ts
@@ -1,46 +1,15 @@
 import express from "express";
 import cors from "cors";
-import fs from "fs";
 import mysql from "mysql";
 import { faker } from "@faker-js/faker";
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-// const app = express();
+const app = express();
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(cors());
-
-// app.get("/api", (req, res, next) => {
-
-//     res.send(fs.readFileSync("./data.json", "utf8"));
-
-// });
-
-// app.post("/api", (req, res) => {
-//     const date = req.body.clickedDate;
-//     const habit = req.body.input;
-
-//     const data = fs.readFileSync("./data.json", "utf8");
-//     const dataObj = JSON.parse(data);
-
-//     if (dataObj[date]) {
-//     dataObj[date].push(habit);
-//     } else {
-//     dataObj[date] = [habit];
-//     }
-
-//     fs.writeFileSync("./data.json", JSON.stringify(dataObj, null, 2), 'utf8')
-
-//     console.log("dataObj:", dataObj);
-//     res.end();
-// });
-
-// const port = +(process.env.PORT || 8443);
-// app.listen(port, () => {
-//     console.log(`Express Server started on port ${port}`);
-// });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -49,31 +18,71 @@ const connection = mysql.createConnection({
   database: "VisonZ_Practice",
 });
 
-const sql = mysql.format("SELECT * FROM user_address", []);
+app.get("/", (req,res) => {
+    res.send("helllo")
+})
 
-// const values = [];
-// for (let i = 0; i < 50; i++) {
-//   values.push([
-//     faker.address.streetAddress(),
-//     faker.address.city(),
-//     faker.address.country(),
-//     i+1
-//   ]);
-// }
+app.get("/api/:date", (req, res) => {
+    const date = req.params.date;
+    console.log('date', date);
+    //format을 써준 이유는????
+    const sql = mysql.format(`SELECT habit FROM habitlist WHERE date = ${date}`, [])
+    connection.query(sql, (err, results) => {
+        if (err) throw err;
+        console.log(`get ${date} results`, results)
 
-// const sql = mysql.format(
-//   "INSERT INTO user_address (street_address, city, country, user_id) VALUES ?",
-//   [values]
-// );
+        const habitList: any = [];
+        results.map((result: any) => {
+            habitList.push(result.habit)
+        })
+        console.log('habitList', habitList);
+        res.send(habitList);
+    })
+})
 
-console.log('result of sql', sql);
+app.get("/api", (req, res, next) => {
 
-connection.query(sql, (err, results) => {
-  if (err) throw err;
-    console.log('results', results);
-  results.forEach((result: any) => {
-    console.log("result.username", result.country);
-  });
+    const sql = mysql.format("SELECT * FROM habitlist", [])
+    console.log('its working11');
+    connection.query(sql, (err, results) => {
+        console.log('its working22');
+        if(err) throw err;
+        res.send(results);
+        // connection.end(); 얘가 있으면 Cannot enqueue Quit after invoking quit. 이런 에러가 남
+    })
 
-  connection.end();
 });
+
+app.post("/api", (req, res) => {
+    const date = req.body.clickedDate;
+    const habit = req.body.input;
+    console.log('date', date);
+    console.log('habit', habit);
+
+    const values = [[habit, date]];
+    const sql = mysql.format("INSERT INTO habitlist (habit, date) VALUES ?", [values]);
+
+    connection.query(sql, (err, results) => {
+        if (err) throw err;
+        console.log('post results', results);
+        // connection.end();
+    } )
+
+    res.end();
+});
+
+const port = +(process.env.PORT || 8443);
+
+app.listen(port, () => {
+  console.log(`Express Server started on port ${port}`);
+});
+
+// connection.query(sql, (err, results) => {
+//   if (err) throw err;
+//   console.log('out results', results);
+//   results.forEach((result: any) => {
+//     console.log("in results", results);
+//   });
+
+//   connection.end();
+// });
